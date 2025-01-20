8ad05e8510b76868b7e73335bc30e18f0185550e
hehehherjcjkb <muhammaddzakihasyim@gmai.com>
2025-01-20 08:55:53 +0800

    update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image';
 import { CiSearch } from "react-icons/ci";
+import { IoMdNotificationsOutline } from "react-icons/io";
 
 function Home
 () {
@@ -21,7 +22,7 @@ function Home
           </div>
         </div>
 
-        <div className="bg-[#950101] w-[870px] h-[160px] mx-[100px] my-[-46px] rounded-lg">
+        <div className="bg-[#950101] w-[870px] h-[160px] mx-[100px] my-[-46px] rounded-xl">
           <div className="text-white text-base mx-[30px] py-[7px]">
             November 2024
           </div>
@@ -39,7 +40,7 @@ function Home
             <div className="text-white text-base  w-[40px]">RAB</div>
           </div>
 
-          <div className="flex justify-between items-center gap-4 mt-4">
+          <div className="flex justify-between -space-x mt-4">
             <div className="text-white text-2xl rounded-lg px-4 py-2  w-[40px] bg-black">
               4
             </div>
@@ -73,18 +74,66 @@ function Home
           </div>
         </div>
 
-        <div className="bg-[#999999] bg-opacity-15 h-[600px] w-[327px] mx-[1210px] my-[-200px] rounded-3xl ">
-
-          <div className="bg-[#999999] bg-opacity-55 w-[245px] h-[42px] rounded-full ml-10"></div>
-          <div className='space-y-4 mx-12 my-48'>
-            <div className="bg-[#999999] text-opacity-100 w-[235px] h-[74px] rounded-2xl">
-              <div className='bg-white w-[42px] h-[52px] rounded-lg'></div>
-            </div>
-            <div className="bg-[#999999] text-opacity-100 w-[235px] h-[74px] rounded-2xl"></div>
-            <div className="bg-[#999999] text-opacity-100 w-[235px] h-[74px] rounded-2xl"></div>
-          </div>
+        <div className="space-y-2 mx-32">
+          <div className="bg-[#950101] bg-opacity-100 w-[439px] h-[44px] mt-32 rounded-lg"></div>
+          <div className="bg-[#950101] bg-opacity-100 w-[439px] h-[44px] mt-32 rounded-lg "></div>
+          <div className="bg-[#950101] bg-opacity-100 w-[439px] h-[44px] mt-32 rounded-lg"></div>
         </div>
 
+        <div className="bg-[#999999] bg-opacity-15 h-[627px] w-[327px] mx-[1210px] my-[-450px] rounded-3xl py-14 px-3 ">
+          <div className='space-y-7'>
+            <div>
+              <form className="relative">
+                <CiSearch className="absolute left-14 top-1/3 text-white"></CiSearch>
+                <input
+                  type="text"
+                  id="Search"
+                  className="bg-[#999999] bg-opacity-55 w-[245px] h-[42px] rounded-full ml-10 text-white placeholder-white pl-12"
+                  placeholder="Pencarian"
+                  required
+                ></input>
+              </form>
+            </div>
+
+            <div className="bg-[#D9D9D9] bg-opacity-100 w-[133px] h-[50px] rounded-lg border-2 border-black mx-[90px] my-16">
+              <div className="text-black font-sans mx-10 my-2">Riwayat</div>
+              <IoMdNotificationsOutline className="ml-3 text-2xl mt-[-30px]"></IoMdNotificationsOutline>
+            </div>
+
+            <h1 className="text-black flex justify-center my-16">
+              Detail Novermber 2024
+            </h1>
+          </div>
+
+          <div className="space-y-4 mx-11 mt-5">
+            <div className="bg-[#999999] text-opacity-100 w-[235px] h-[74px] rounded-xl flex items-center px-2">
+              <div className="bg-white w-[42px] h-[52px] rounded-md ">
+                <div className="text-black flex justify-center py-3 font-medium">
+                  18
+                </div>
+              </div>
+              <h1 className="text-white font-normal mx-3">Sakit</h1>
+            </div>
+
+            <div className="bg-[#999999] text-opacity-100 w-[235px] h-[74px] rounded-xl flex items-center px-2">
+              <div className="bg-white w-[42px] h-[52px] rounded-md ">
+                <div className="text-black flex justify-center py-3 font-medium">
+                  36
+                </div>
+              </div>
+              <h1 className="text-white font-normal mx-3">Pulang</h1>
+            </div>
+
+            <div className="bg-[#999999] text-opacity-100 w-[235px] h-[74px] rounded-xl flex items-center px-2">
+              <div className="bg-white w-[42px] h-[52px] rounded-md ">
+                <div className="text-black flex justify-center py-3 font-medium">
+                  29
+                </div>
+              </div>
+              <h1 className="text-white font-normal mx-3">Keluar</h1>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
